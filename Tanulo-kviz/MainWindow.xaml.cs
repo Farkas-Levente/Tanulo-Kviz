8c3862cec747e36a7f758a90db3a7d89df38bd5b
Farkas-Levente <89079547+Farkas-Levente@users.noreply.github.com>
2022-02-26 10:56:11 +0100

    A kvíz gombok és Ui működése
    A gombok és UI működése kész

--- Tanulo-kviz/MainWindow.xaml.cs
+++ Tanulo-kviz/MainWindow.xaml.cs
@@ -118,5 +118,345 @@ namespace Tanulo_kviz
                 valaszok.Add(valasz4);
             }
         }
+        private void TargyBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            temakorBox.Items.Clear();
+            temakorBox.Items.Clear();
+            selectedTargy = null;
+            selectedTema = null;
+
+            Tantargy targy = null;
+            string selectedTargyString = targyBox.SelectedItem.ToString();
+            tantargyNyilvantarto.TryGetValue(selectedTargyString, out targy);
+
+            temakorBox.IsEnabled = false;
+
+            if (targy == null) return;
+
+            temakorBox.IsEnabled = true;
+
+            selectedTargy = targy;
+            foreach (Tema tema in targy.temak)
+            {
+                temakorBox.Items.Add(tema.nev);
+            }
+        }
+
+        private void TemaBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+            if (temakorBox.SelectedItem == null || temakorBox.Items.Count <= 0) { return; }
+            Tema tema = null;
+            string selectedTemaString = temakorBox.SelectedItem.ToString();
+
+            selectedTargy.temaNyilvantarto.TryGetValue(selectedTemaString, out tema);
+
+            if (tema == null) return;
+
+            selectedTema = tema;
+        }
+
+        private void General_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedTema == null && selectedTargy == null)
+            {
+                MessageBox.Show("Kérem válasszon tantárgyat és témakört!");
+                return;
+            }
+            else if (selectedTema == null)
+            {
+                MessageBox.Show("Kérem válasszon témakört!");
+                return;
+            }
+            else if (selectedTargy == null)
+            {
+                MessageBox.Show("Kérem válasszon tantárgyat!");
+                return;
+            }
+
+            kvizLap.Visibility = Visibility.Visible;
+            foLap.Visibility = Visibility.Hidden;
+
+            tantargy.Content = selectedTargy.nev;
+            temakor.Content = selectedTema.nev;
+            GeneralKerdesSor();
+
+            foreach (RadioButton button in buttons)
+            {
+                if (button.Content.ToString() == currentKerdes.helyesValasz)
+                {
+                    button.Foreground = Brushes.Black;
+                }
+                else
+                {
+                    button.Foreground = Brushes.Black;
+                }
+                button.IsEnabled = true;
+            }
+            kiertekelt = false;
+        }
+
+
+        private void GeneralKerdesSor()
+        {
+            Random random = new Random();
+            List<Kerdes> osszesKerdes = selectedTema.kerdesek;
+            List<Kerdes> tempKerdesek = selectedTema.kerdesek;
+            for (int i = 0; i < 10; i++)
+            {
+                int randomIndex = random.Next(0, tempKerdesek.Count);
+                betoltottKerdesek.Add(tempKerdesek[randomIndex]);
+                tempKerdesek.RemoveAt(randomIndex);
+            }
+            BetoltKerdes(betoltottKerdesek[0]);
+            oldalIndex = 0;
+        }
+
+        private void BetoltKerdes(Kerdes ujKerdes)
+        {
+            Random random = new Random();
+            List<string> tempValasz = new List<string>();
+            currentKerdes = ujKerdes;
+            foreach (var item in ujKerdes.valaszok)
+            {
+                tempValasz.Add(item);
+            }
+            List<RadioButton> tempButton = new List<RadioButton>();
+            tempButton = buttons;
+
+            if (ujKerdes.sorrend.Count <= 0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+
+                    int randomIndex = random.Next(0, tempValasz.Count);
+
+                    buttons[i].Content = tempValasz[randomIndex];
+                    ujKerdes.sorrend.Add(tempValasz[randomIndex]);
+                    tempValasz.RemoveAt(randomIndex);
+
+
+
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    buttons[i].Content = ujKerdes.sorrend[i];
+                }
+            }
+            foreach (RadioButton but in buttons)
+            {
+                if (but.Content.ToString() == ujKerdes.valasztott && ujKerdes.valasztott != null)
+                {
+                    but.IsChecked = true;
+                }
+            }
+
+            kerdes.Content = ujKerdes.kerdes;
+            oldalIndex = betoltottKerdesek.IndexOf(ujKerdes);
+            oladalJelzo.Content = $"{oldalIndex + 1}/10";
+            foreach (Button button in oldalValtoGombok)
+            {
+                button.Background = Brushes.White;
+            }
+            oldalValtoGombok[oldalIndex].Background = Brushes.Aqua;
+
+            if (kiertekelt)
+            {
+                foreach (RadioButton button in buttons)
+                {
+                    if (button.Content.ToString() == ujKerdes.helyesValasz)
+                    {
+                        button.Foreground = Brushes.Green;
+                    }
+                    else
+                    {
+                        button.Foreground = Brushes.Red;
+                    }
+                    button.IsEnabled = false;
+
+                }
+            }
+
+        }
+
+        private void Kovetkezo_Oldal(object sender, RoutedEventArgs e)
+        {
+            if (oldalIndex + 1 >= betoltottKerdesek.Count) return;
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[oldalIndex + 1]);
+
+        }
+
+        private void elozoLap_Betolt(object sender, RoutedEventArgs e)
+        {
+            if (oldalIndex <= 0) return;
+            BetoltKerdes(betoltottKerdesek[oldalIndex - 1]);
+        }
+
+
+
+        private void Valasz1_Checked(object sender, RoutedEventArgs e)
+        {
+            currentKerdes.valasztott = valasz1.Content.ToString();
+            valasz1.Background = Brushes.LightGray;
+            valasz2.Background = Brushes.Transparent;
+            valasz3.Background = Brushes.Transparent;
+            valasz4.Background = Brushes.Transparent;
+
+        }
+
+        private void Valasz2_Checked(object sender, RoutedEventArgs e)
+        {
+            currentKerdes.valasztott = valasz2.Content.ToString();
+            valasz2.Background = Brushes.LightGray;
+            valasz1.Background = Brushes.Transparent;
+            valasz3.Background = Brushes.Transparent;
+            valasz4.Background = Brushes.Transparent;
+
+        }
+
+        private void Valasz3_Checked(object sender, RoutedEventArgs e)
+        {
+            currentKerdes.valasztott = valasz3.Content.ToString();
+            valasz3.Background = Brushes.LightGray;
+            valasz2.Background = Brushes.Transparent;
+            valasz1.Background = Brushes.Transparent;
+            valasz4.Background = Brushes.Transparent;
+        }
+
+        private void Valasz4_Checked(object sender, RoutedEventArgs e)
+        {
+            currentKerdes.valasztott = valasz4.Content.ToString();
+            valasz4.Background = Brushes.LightGray;
+            valasz2.Background = Brushes.Transparent;
+            valasz3.Background = Brushes.Transparent;
+            valasz1.Background = Brushes.Transparent;
+
+        }
+
+        private void oldal_1_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[0]);
+        }
+
+        private void Nullazas()
+        {
+            valasz1.IsChecked = false;
+            valasz2.IsChecked = false;
+            valasz3.IsChecked = false;
+            valasz4.IsChecked = false;
+            valasz1.Background = Brushes.Transparent;
+            valasz2.Background = Brushes.Transparent;
+            valasz3.Background = Brushes.Transparent;
+            valasz4.Background = Brushes.Transparent;
+        }
+
+        private void oldal_2_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[1]);
+        }
+
+        private void oldal_10_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[9]);
+        }
+
+        private void oldal_9_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[8]);
+        }
+
+        private void oldal_8_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[7]);
+        }
+
+        private void oldal_7_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[6]);
+        }
+
+        private void oldal_6_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[5]);
+        }
+
+        private void oldal_5_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[4]);
+        }
+
+        private void oldal_4_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[3]);
+        }
+
+        private void oldal_3_Click(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            BetoltKerdes(betoltottKerdesek[2]);
+        }
+
+        private void kiertekeles(object sender, RoutedEventArgs e)
+        {
+            kiertekelt = true;
+
+            foreach (RadioButton button in buttons)
+            {
+                if (button.Content.ToString() == currentKerdes.helyesValasz)
+                {
+                    button.Foreground = Brushes.Green;
+                }
+                else
+                {
+                    button.Foreground = Brushes.Red;
+                }
+                button.IsEnabled = false;
+
+            }
+
+            int helyesValaszok = 0;
+            foreach (Kerdes kerdes in betoltottKerdesek)
+            {
+                if (kerdes.valasztott == kerdes.helyesValasz)
+                {
+                    helyesValaszok++;
+                }
+            }
+
+            MessageBox.Show($"{helyesValaszok}/10 pontod lett.");
+
+            kilepes.Visibility = Visibility.Visible;
+        }
+
+        private void kilep(object sender, RoutedEventArgs e)
+        {
+            Nullazas();
+            betoltottKerdesek.Clear();
+            oldalIndex = 0;
+
+            kvizLap.Visibility = Visibility.Hidden;
+            foLap.Visibility = Visibility.Visible;
+
+            kilepes.Visibility = Visibility.Hidden;
+            selectedTema = null;
+            selectedTargy = null;
+            Close();
+
+
+        }
     }
 }
